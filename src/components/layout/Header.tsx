e15346bd05d4d08ce76a4db261b6929da668cdfb
Faiz Authar <faizauthar@gmail.com>
2023-08-10 14:34:21 +0700

    frontend: header: Add icon for beranda

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,4 @@
+import { Home } from 'lucide-react';
 import * as React from 'react';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
@@ -11,9 +12,14 @@ export default function Header() {
   return (
     <header className='sticky top-0 z-50 bg-white'>
       <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
+        <UnstyledLink
+          href='/'
+          className='flex-start flex items-center gap-1 font-bold hover:text-gray-600'
+        >
+          <Home />
           Beranda
         </UnstyledLink>
+
         <nav>
           <ul className='flex items-center justify-between space-x-4'>
             {links.map(({ href, label }) => (
