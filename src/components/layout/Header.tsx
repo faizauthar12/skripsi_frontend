f7d184e51da6a5dae539ad3cea4346de38af32d0
Faiz Authar <faizauthar@gmail.com>
2023-08-10 22:30:51 +0700

    frontend: header: Add cart icon and the badge counter

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,34 @@
-import { Home } from 'lucide-react';
+import { getCookie } from 'cookies-next';
+import { Home, ShoppingBag } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import * as React from 'react';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
 
-const links = [
-  { href: '/produk', label: 'Produk' },
-  { href: '/keranjang', label: 'Keranjang' },
-];
+import { CartCookie } from '@/types/cart/CartCookie';
 
 export default function Header() {
+  const router = useRouter();
+
+  const [cartCookie, setCartCookie] = React.useState<CartCookie[]>([]);
+
+  const handleGetCurrentCart = React.useCallback(() => {
+    if (process.env.NODE_ENV == 'development') {
+      console.log('Header: handleGetCurrentCart');
+    }
+
+    const currentCart = getCookie('cart') as string;
+
+    if (currentCart) {
+      const currentCartParsed: CartCookie[] = JSON.parse(currentCart);
+      setCartCookie(currentCartParsed);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    handleGetCurrentCart();
+  }, [handleGetCurrentCart]);
+
   return (
     <header className='sticky top-0 z-50 bg-white'>
       <div className='layout flex h-14 items-center justify-between'>
@@ -22,13 +42,18 @@ export default function Header() {
 
         <nav>
           <ul className='flex items-center justify-between space-x-4'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
-                </UnstyledLink>
-              </li>
-            ))}
+            <li>
+              <UnstyledLink href='/produk' className='hover:text-gray-600'>
+                Produk
+              </UnstyledLink>
+            </li>
+
+            <li className='relative rounded-full border-2 border-transparent px-2 py-3 transition duration-150 ease-in-out hover:opacity-50'>
+              <ShoppingBag onClick={() => router.push('/keranjang')} />
+              <div className='absolute right-0 top-0 flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-2 text-xs text-white'>
+                {cartCookie.length}
+              </div>
+            </li>
           </ul>
         </nav>
       </div>
