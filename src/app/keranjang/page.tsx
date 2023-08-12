ea2a7c12d91e87ce94ab73b151ce8177e534091e
Faiz Authar <faizauthar@gmail.com>
2023-08-12 13:19:39 +0700

    frontend: keranjang: remove setCheckoutButton at handleGetCurrentCart, let the useEffect set it!

--- src/app/keranjang/page.tsx
+++ src/app/keranjang/page.tsx
@@ -27,7 +27,6 @@ export default function CartPage() {
     if (currentCart) {
       const currentCartParsed: CartCookie[] = JSON.parse(currentCart);
       setCartCookie(currentCartParsed);
-      setCheckoutButton(true);
     }
   }, []);
 
